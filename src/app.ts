ecaa23099147cb0d507674c9d47c11d0862cda64
FoseFx <fosefx@pm.me>
2019-09-29 18:50:57 +0200

    Adds cors and error fallback

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import * as multer from "multer";
 import * as grcp from "grpc";
 import {MLBackendClient} from "../static_codegen/protos/ml_grpc_pb";
 import * as fs from "fs";
+import * as cors from 'cors';
+import { NextFunction, Response, ErrorRequestHandler } from "express";
 
 // get certificate
 const path = "./secret/certificate.crt";
@@ -18,6 +20,8 @@ export const app = express();
 
 export const upload = multer({storage: multer.memoryStorage()});
 
+app.use(cors());
+
 app.use(helmet())
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -28,3 +32,13 @@ app.use(bodyParser.json())
 
 app.post("/", upload.single('image') , classify);
 app.get("*", (_, res) => res.status(405).json({error: "Method not allowed", descr: "Use POST / with the body of image=<your image file>"}).end());
+
+// @ts-ignore
+const errorHandler: ErrorRequestHandler = (err: any, req: Request, res: Response, _next: NextFunction) => {
+    console.error('Error catched fetching', req.url);
+    console.error(err);
+    console.debug(req);
+    return res.status(500).json({error: 'Internal Server Error'}).end();
+};
+
+app.use(errorHandler);
